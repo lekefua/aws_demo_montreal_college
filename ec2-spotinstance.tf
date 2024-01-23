6fe8dd8f60e5a71dce18098ec69498d378e99774
Ruthkwe08 <146134486+Ruthkwe08@users.noreply.github.com>
2024-01-22 20:28:37 -0500

    Update ec2-spotinstance.tf

--- ec2-spotinstance.tf
+++ ec2-spotinstance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "example" { 
   ami = "ami-for practice" 
   instance_type = "t2.micro" 
-spot_price = "0.01" 
-tags = { 
-Name = "akwesi-instance" 
-} 
+  spot_price = "0.01" 
+  tags = { 
+    Name = "akwesi-instance" 
+  } 
 }
