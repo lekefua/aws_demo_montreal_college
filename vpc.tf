fa2393b4b597036fa5e014e3f1c792ff404eb86b
Ruthkwe08 <146134486+Ruthkwe08@users.noreply.github.com>
2024-01-24 18:56:04 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,14 +6,14 @@ resource "aws_vpc" "myvpc"{
     }
 }
 
- 2: Create a public subnet
+#Create a public subnet
 resource "aws_subnet" "PublicSubnet"{
     vpc_id = aws_vpc.myvpc.id
     availability_zone = "us-east-1a"
     cidr_block = "10.0.1.0/24"
 }
 
- 3 : create a private subnet
+#create a private subnet
 resource "aws_subnet" "PrivSubnet"{
     vpc_id = aws_vpc.myvpc.id
     cidr_block = "10.0.2.0/24"
@@ -22,12 +22,12 @@ resource "aws_subnet" "PrivSubnet"{
 }
 
 
- 4 : create IGW
+#create IGW
 resource "aws_internet_gateway" "myIgw"{
     vpc_id = aws_vpc.myvpc.id
 }
 
- 5 : route Tables for public subnet
+#route Tables for public subnet
 resource "aws_route_table" "PublicRT"{
     vpc_id = aws_vpc.myvpc.id
     route {
@@ -37,7 +37,7 @@ resource "aws_route_table" "PublicRT"{
 }
  
 
- 7 : route table association public subnet 
+#route table association public subnet 
 resource "aws_route_table_association" "PublicRTAssociation"{
     subnet_id = aws_subnet.PublicSubnet.id
     route_table_id = aws_route_table.PublicRT.id
