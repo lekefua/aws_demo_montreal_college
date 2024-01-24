51ea36fe15d188ea7519acb6c7dcb11f0633f28a
lekefua <145169940+lekefua@users.noreply.github.com>
2024-01-25 00:41:14 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -15,3 +15,10 @@ variable "instance_type"{
     type =string
     default="t3.micro"
 }
+variable "vpc_name" {
+  description = "Name of the VPC"
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+}
