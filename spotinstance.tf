c9ee85e79fc60c61b3c9585cfc4636615e5e45e9
lekefua <145169940+lekefua@users.noreply.github.com>
2024-01-24 05:26:35 +0100

    Update spotinstance.tf

--- spotinstance.tf
+++ spotinstance.tf
@@ -5,8 +5,7 @@ resource "aws_instance" "spot_instance" {
   instance_type = "t2.micro"  # Replace with your desired instance type
   key_name      = "your-key-pair-name"  # Replace with your key pair name
 
-  spot_price    = "0.03"  # Replace with your desired maximum bid price
-  lifecycle     = "spot"
+  spot_price    = "0.03"  
 
   tags = {
     Name = "SpotInstance-${count.index + 1}"
