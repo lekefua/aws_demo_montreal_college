43421d69e7258d6aa0be5cc31243da269677d744
lekefua <145169940+lekefua@users.noreply.github.com>
2024-01-24 05:27:57 +0100

    Update spotinstance.tf

--- spotinstance.tf
+++ spotinstance.tf
@@ -4,9 +4,6 @@ resource "aws_instance" "spot_instance" {
   ami           = "ami-xxxxxxxxxxxxxxxxx"  # Replace with your desired AMI ID
   instance_type = "t2.micro"  # Replace with your desired instance type
   key_name      = "your-key-pair-name"  # Replace with your key pair name
-
-  spot_price    = "0.03"  
-
   tags = {
     Name = "SpotInstance-${count.index + 1}"
   }
