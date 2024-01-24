74ffcd688f3ecc2ba6e4131b07250c8fe5213808
Ruthkwe08 <146134486+Ruthkwe08@users.noreply.github.com>
2024-01-24 18:46:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # main.tf 
 
 module "my_vpc" { 
-  source = "./vpc.tf./path/to/your/module" # Update the path accordingly 
+  source = "./vpc.tf/path/to/your/module" # Update the path accordingly 
   
     vpc_name = "my-vpc" 
     cidr_block = "10.0.0.0/16" # Update with your desired CIDR block }
