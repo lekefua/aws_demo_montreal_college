7ae9ce2c03afba62a25f47699a1b3b3c7ac66509
lekefua <145169940+lekefua@users.noreply.github.com>
2024-01-25 02:11:57 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,5 @@
 # Create VPC Terraform Module
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  #version = "2.78.0"
-  version = "3.0.0"
- 
+
   # VPC Basic Details
   name = "${local.name}-${var.vpc_name}"
   cidr = var.vpc_cidr_block
