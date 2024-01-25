d223658319a5a2328460d7033817dbba832bccd7
lekefua <145169940+lekefua@users.noreply.github.com>
2024-01-25 02:08:03 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -15,3 +15,19 @@ variable "instance_type"{
     type =string
     default="t3.micro"
 }
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "CIDR blocks for public subnets"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "CIDR blocks for private subnets"
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
